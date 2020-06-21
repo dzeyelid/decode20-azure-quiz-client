80ff1a18ea4061174097dd39881e475690741da5
Kazumi OHIRA <dzeyelid@gmail.com>
2020-06-21 12:44:22 +0900

    Add APIs.

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:7071',
+        ws: true,
+        changeOrigin: true,
+      }
+    }
+  },
   transpileDependencies: [
     'vuetify',
   ],
